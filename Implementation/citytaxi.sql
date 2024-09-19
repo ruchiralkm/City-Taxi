5c7ab4e8702e8534029bfdfc7a3d8a82bfe6e653
hareethtahani@gmail.com <hareethtahani@gmail.com>
2024-09-19 22:08:13 +0530

    Update citytaxi.sql
    new sql

--- Implementation/citytaxi.sql
+++ Implementation/citytaxi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2024 at 05:42 PM
+-- Generation Time: Sep 19, 2024 at 06:35 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -44,6 +44,28 @@ INSERT INTO `passenger` (`passengerID`, `name`, `mobile`, `email`, `password`) V
 (2, 'SAM', 1236547890, 'SAM@RAT.COM', '$2y$10$xfRpo/8gb2.xiVc57XLjO.LCMHelN5z80SwOnb4Cy2s'),
 (3, 'asd', 2147483647, 'asd@gmail.com', '$2y$10$HrQAhkJv/iB4dd3quMjP0.U.eBVj5ictcr0eMOfta6Rd.JlgkOQ6W');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ride`
+--
+
+CREATE TABLE `ride` (
+  `rideID` int(11) NOT NULL,
+  `pickupLocation` varchar(255) NOT NULL,
+  `dropLocation` varchar(255) NOT NULL,
+  `distance` float NOT NULL,
+  `fare` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ride`
+--
+
+INSERT INTO `ride` (`rideID`, `pickupLocation`, `dropLocation`, `distance`, `fare`) VALUES
+(1, '79.854174,6.938961', '80.635015,7.293021', 0, 0),
+(2, '79.854174,6.938961', '80.635015,7.293021', 141.05, 143.05);
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +77,12 @@ ALTER TABLE `passenger`
   ADD PRIMARY KEY (`passengerID`);
 
 --
+-- Indexes for table `ride`
+--
+ALTER TABLE `ride`
+  ADD PRIMARY KEY (`rideID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +91,12 @@ ALTER TABLE `passenger`
 --
 ALTER TABLE `passenger`
   MODIFY `passengerID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ride`
+--
+ALTER TABLE `ride`
+  MODIFY `rideID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
