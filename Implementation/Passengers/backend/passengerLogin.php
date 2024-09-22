8f22476fe13a5e756725e02080c7df7eacac3fc1
Ruchira Kaluarachchi <151660406+ruchiralkm@users.noreply.github.com>
2024-09-22 12:36:44 +0530

    Update

--- Implementation/Passengers/backend/passengerLogin.php
+++ Implementation/Passengers/backend/passengerLogin.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -40,11 +52,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             else 
             {
                 echo "<h3>Incorrect password. Please try again.</h3>";
+                // Redirect to the passenger dashboard or another page
+                header("Location: ../SuccessError/error.html");
+                exit();
             }
         } 
         else 
         {
             echo "<h3>No account found with that email. Please sign up first.</h3>";
+            // Redirect to the passenger dashboard or another page
+            header("Location: ../SuccessError/error.html");
         }
         
         // Close the database connection
