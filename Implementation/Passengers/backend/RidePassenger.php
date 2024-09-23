3ddac413f3006e190a84ccc35d565802f97c6ce9
Ruchira Kaluarachchi <151660406+ruchiralkm@users.noreply.github.com>
2024-09-23 22:04:03 +0530

    Update pages

--- Implementation/Passengers/backend/RidePassenger.php
+++ Implementation/Passengers/backend/RidePassenger.php
@@ -9,8 +9,11 @@
     <!-- =====RidePassenger SCSS File ===== -->
     <link rel="stylesheet" href="Sass/RidePassenger.min.css" />
 
-    <!-- =====RidePassenger SCSS File ===== -->
+    <!-- =====Vehicle Card SCSS File ===== -->
     <link rel="stylesheet" href="VehicleCardSass/vehicleCards.min.css" />
+
+    <!-- =====Driver Card SCSS File ===== -->
+    <link rel="stylesheet" href="DriverCardSass/DriverCardSass.min.css" />
 
     <!-- Font Awesome (for icons) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
@@ -29,17 +32,16 @@
     <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.js"></script>
     <link href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.css" rel="stylesheet">
 
-    <h1>Ride Section</h1>
-
+    <br><br>
     <div class="content">
         <!-- Left section -->
         <div class="left">
             <h2>Trip Details</h2>
             <!-- Ride Booking Form -->
             <form action="processRide.php" method="post" id="rideForm">
-                <div id="fareDetails">
+                <div id="fareDetails" class="fareDetails">
                     <p><strong>Distance:</strong> <span id="distanceDisplay"></span></p>
-                    <p><strong>Fare:</strong> $<span id="fareDisplay"></span></p>
+                    <p><strong>Price:</strong> $<span id="fareDisplay"></span></p>
 
                     <!-- Hidden Inputs to Store Ride Details -->
                     <input type="hidden" id="distance" name="distance">
@@ -72,6 +74,7 @@
                           alt=""
                         />
                         <label for="bike">Bike</label>
+                        <label for="bike">LKR 200</label>
                       </div>
                     </div>   
                     
@@ -90,6 +93,7 @@
                           alt=""
                         />
                         <label for="threewheel">Threewheel</label>
+                        <label for="bike">LKR 200</label>
                       </div>
                     </div>
 
@@ -103,6 +107,7 @@
                           alt=""
                         />
                         <label for="car">Car</label>
+                        <label for="bike">LKR 200</label>
                       </div>
                     </div>
 
@@ -116,19 +121,101 @@
                           alt=""
                         />
                         <label for="van">Van</label>
+                        <label for="bike">LKR 200</label>
                       </div>
                     </div>
 
                     
                   </div>
                 </div>
+
+
+
+                <!-- Select Driver -->
+                <h2 style="margin-top:-70px;">Select your driver</h2>
+                <div class="card-container">
+
+                  <!-- 1st Driver -->
+                  <input type="radio" id="card1" name="card" class="card-input" />
+                  <label for="card1" class="card">
+                    <img
+                      src="https://www.f1fantasytracker.com/Images/Drivers/2021/Headshots/Verstappen.png"
+                      alt=""
+                    />
+                    <div class="card-content">
+                      <h3>Daniel Brayan</h3>
+                      <p>Tel: 0777123456</p>
+                      <p>Age: 26</p>
+                      <p>Catogory: Bike</p>
+
+                      <!-- Star rating -->
+                      <div class="star-rating">
+                        <span class="star filled">★</span>
+                        <span class="star filled">★</span>
+                        <span class="star filled">★</span>
+                        <span class="star filled">★</span>
+                        <span class="star">☆</span>
+                      </div>
+
+                    </div>
+                  </label>
+
+                  <!-- 2nd Driver -->
+                  <input type="radio" id="card2" name="card" class="card-input" />
+                  <label for="card2" class="card">
+                    <img
+                      src="https://o.quizlet.com/Iu7WzrIMeFItkAv06hDyaQ.jpg"
+                      alt=""
+                    />
+                    <div class="card-content">
+                      <h3>John Cena</h3>
+                      <p>Tel: 0777123456</p>
+                      <p>Age: 32</p>
+                      <p>Catogory: Car</p>
+
+                      <!-- Star rating -->
+                      <div class="star-rating">
+                        <span class="star filled">★</span>
+                        <span class="star filled">★</span>
+                        <span class="star filled">★</span>
+                        <span class="star">☆</span>
+                        <span class="star">☆</span>
+                      </div>
+
+                    </div>
+                  </label>
+
+                  <!-- 3rd Driver -->
+                  <input type="radio" id="card3" name="card" class="card-input" />
+                  <label for="card3" class="card">
+                    <img
+                      src="https://tiermaker.com/images/media/hero_images/2024/17244409/2025-f1-driver-line-up-prediction-17244409/172444091718791306.png"
+                      alt=""
+                    />
+                    <div class="card-content">
+                      <h3>Daniel Brayan</h3>
+                      <p>Tel: 0777123456</p>
+                      <p>Age: 20</p>
+                      <p>Catogory: Van</p>
+
+                      <!-- Star rating -->
+                      <div class="star-rating">
+                        <span class="star filled">★</span>
+                        <span class="star filled">★</span>
+                        <span class="star">☆</span>
+                        <span class="star">☆</span>
+                        <span class="star">☆</span>
+                      </div>
+
+                    </div>
+                  </label>
+                </div>
                  
-
-
-
 
                 <button type="submit" id="bookRideBtn">Book Ride</button>
             </form>
+
+            <p class="footerP">© 2024 City-Taxi(pvt). All rights reserved.</p>
         </div>
 
         <!-- Right section -->
