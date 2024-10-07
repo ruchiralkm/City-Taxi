5482f90178809ca65bfd3e7d9ff6b983e6de94b4
Ruchira Kaluarachchi <151660406+ruchiralkm@users.noreply.github.com>
2024-10-07 22:04:48 +0530

    update

--- Implementation/success.php
+++ Implementation/success.php
@@ -1,12 +1,35 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title>Stripe Example</title>
+<html lang="en">
+  <head>
     <meta charset="UTF-8" />
-</head>
-<body>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Registration Confirmation</title>
 
-    <h1>Stripe Example</h1>
-    <p>Thank you for your payment!</p>
-</body>
+    <!-- Main success scss file -->
+    <link rel="stylesheet" href="Sass/success.min.css" />
+  </head>
+  <body>
+    <!--* hero section *-->
+    <div class="conn">
+      <div class="container">
+        <div class="header">
+          <img
+            src="https://img.icons8.com/?size=100&id=a4l6bA9mSmBh&format=png&color=40C057"
+            alt="Checkmark"
+            class="checkmark"
+          />
+          <h1>Your payment is succefully</h1>
+        </div>
+        <p>
+          Thank you for your payment. Stay tuned with City-Taxi(PVT)Ltd.
+        </p>
+        <p>
+          For further information, please go to the
+          <a href="About/about.html">documentation</a>.
+        </p>
+        <br />
+        <button class="backbtn" onclick="window.history.back()">Back</button>
+      </div>
+    </div>
+  </body>
 </html>
